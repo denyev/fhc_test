8ff3ce987b277da93c9d5342152ec059bc44f74e
denyev <denis.yevtushenko@yandex.ru>
2017-01-26 02:56:38 +0300

    Added data in IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(){
-      return view('index');
+      $data = [
+        'title' => 'Тестовое задание',
+      ];
+      return view('index', $data);
     }
 }
