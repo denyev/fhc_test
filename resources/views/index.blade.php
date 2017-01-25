ca7da5a369395e1d983cfbb94f60789f8fdcd14c
denyev <denis.yevtushenko@yandex.ru>
2017-01-26 02:57:30 +0300

    Added the template for views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>{{ isset($title) ? $title : 'Тестовое задание'}}</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+<div class="container">
+  <div class="row text-center">
+    <h1>Тестовое задание</h1>
+  </div>
+</div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
