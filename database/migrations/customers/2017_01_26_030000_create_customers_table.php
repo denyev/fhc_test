a70a17df4c97bb93440af2d9b79e8803cc316fa1
denyev <denis.yevtushenko@yandex.ru>
2017-01-26 03:58:46 +0300

    Added migrations for table customers

---
+++ database/migrations/customers/2017_01_26_030000_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id', 32);
+            $table->string('name', 128);
+            $table->string('tel', 32);
+            $table->string('email', 255)->unique();
+            $table->string('club', 128);
+            $table->string('perm', 32);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
